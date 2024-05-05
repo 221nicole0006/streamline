8d1cbdd09aad4c72040af6d814acc00bd7a60de4
221nicole0006 <168340152+221nicole0006@users.noreply.github.com>
2024-05-05 14:40:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 
-# Title
-st.title("Input and Output Boxes")
+# Title and Image for Introduction
+st.title("Emotion Detection based on Text and Emojis")
+st.image("intro.png", use_column_width=True)
 
 # Input box
-user_input = st.text_input("Emotion Detection based on Text and Emojis")
+user_input = st.text_input("Enter some text:")
 
 # Output box
 st.write("Output:", user_input)
+
+# Image for Features
+st.image("features.png", use_column_width=True)
